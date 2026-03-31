942282ac9be92adfb6064527fa4da75967917cd6
DanielTseng2004 <112913131+DanielTseng2004@users.noreply.github.com>
2026-03-31 17:12:17 +0800

    nnn

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import "element-plus/dist/index.css";
 import * as ElementPlusIconsVue from "@element-plus/icons-vue";
 import { createPinia } from "pinia";
 import "./assets/all.css";
+import "element-plus/theme-chalk/dark/css-vars.css";
 const app = createApp(App);
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component);
